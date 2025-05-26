ab3cd728ceeb0e1a896c98d6f66372e7b6521be5
harshy23 <0harshmishra0@gmail.com>
2025-05-26 17:29:07 +0530

    Update urls.py

--- anime/urls.py
+++ anime/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/",views.new_user,name="new_user"),
+    path("register/",views.new_user,name="new_user/"),
     path("search/",views.anime_search,name="anime_search"),
     path("recommendations/",views.anime_recommendation,name="anime_recommendation"),
     path("preferences/",views.user_preference_genre,name="user_preference_genre"),
